f77cc4fe1e5e9d3a7be23a06b3dd3b6f0f497139
Aleanny Burgos <aleannyburgos@gmail.com>
2025-05-27 12:02:02 -0400

    Add files via upload

---
+++ 4bitwidemux4to1/mux4to1.v
@@ -0,0 +1,14 @@
+module mux4to1(
+	input [3:0] a, b, c, d,
+	input [1:0] sel,
+	output reg [3:0] out
+);
+	always @(*) begin
+		case(sel)
+			2'b00: out = a;
+			2'b01: out = b;
+			2'b10: out = c;
+			2'b11: out = d;
+		endcase
+	end
+endmodule
